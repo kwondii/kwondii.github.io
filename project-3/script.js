442d6af9912fa25d06636501f29dd22aa5b9d835
kwondii <madison.kwon@cooper.edu>
2025-04-07 18:58:25 -0400

    random cup

--- project-3/script.js
+++ project-3/script.js
@@ -1,1 +1,6 @@
 let rangeInput = document.getElementById("range-input")
+
+let contain = document.getElementById("bluecup");
+let containers = ["images/bluecup.PNG", "images/cone.PNG", "images/greencup.PNG", "images/purplecup.PNG", "images/redcup.PNG", "images/stripecup.PNG", "images/yellowcup.PNG"];
+let randomContainers = Math.floor(Math.random() * containers.length);
+contain.src = containers[randomContainers];
